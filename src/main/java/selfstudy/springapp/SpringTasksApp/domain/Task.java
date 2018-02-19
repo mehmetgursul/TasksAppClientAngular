1f6fb6cf034b2dc7e60e696d9749c618b45ae623
Mehmet M. Gursul <mehmetgursul@gmail.com>
2018-02-19 12:40:13 +0300

    Lombok removed

--- src/main/java/selfstudy/springapp/SpringTasksApp/domain/Task.java
+++ src/main/java/selfstudy/springapp/SpringTasksApp/domain/Task.java
@@ -8,12 +8,7 @@ import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 @Entity
-@Data
-@AllArgsConstructor
 public class Task {
 	@Id
 	@GeneratedValue
@@ -28,13 +23,44 @@ public class Task {
 	public Task() 
 	{
 		
-	}
-	
+	}	
 	
 	public Task(Long id, String name, LocalDate dueDate, Boolean completed) {
 		this.id = id;
 		this.name = name;
 		this.dueDate = dueDate;
 		this.completed = completed;
-	}	
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Boolean getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
+	}			
 }
